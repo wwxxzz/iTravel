0fde24e4f189c9c90e7993c2ef8ceccc9efd2629
pigzhu0919 <434415980@qq.com>
2017-09-07 14:54:29 +0800

    itravel

--- app/src/main/java/com/example/aa/itravel/activity/Home_activity.java
+++ app/src/main/java/com/example/aa/itravel/activity/Home_activity.java
@@ -1,26 +1,33 @@
 package com.example.aa.itravel.activity;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 import com.example.aa.itravel.R;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+;
 
 /**
  * Created by aa on 2017/9/5.
  */
 @ContentView(R.layout.home)
-public class Home_activity extends Activity{
+public class Home_activity extends FragmentActivity {
         //private Button friendBt;
         //private Button messageBt;
         private Context mContext;
+
+    @ViewInject(R.id.title_bar_name)
+    private TextView textView;
 
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -28,6 +35,14 @@ public class Home_activity extends Activity{
             //setContentView(R.layout.home);
             mContext =this;
             x.view().inject(this);
+
+            textView.setText("首页推荐");
+
+            /*FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction tt =fm.beginTransaction();
+            tt.replace(R.id.fragment_container,new Banner());
+            tt.commit();*/
+
         }
     @Event(value = {R.id.bt_friend,R.id.bt_message})
     private void event(View view){
