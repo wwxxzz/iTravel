fc79a685ce103cfdcb88d96303c85ef0ddf86964
syq213150489 <3132059957@qq.com>
2017-09-09 11:05:23 +0800

    FriendTop

--- app/src/main/java/com/example/aa/itravel/activity/Friend_activity.java
+++ app/src/main/java/com/example/aa/itravel/activity/Friend_activity.java
@@ -4,29 +4,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.aa.itravel.R;
+import com.example.aa.itravel.adapter.FriendTopAdapter;
+import com.example.aa.itravel.fragment.FriendTopFragment;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.example.aa.itravel.R.id.bottombar;
 import static com.example.aa.itravel.R.id.button_friend;
-
-import static com.example.aa.itravel.R.id.button_home;
 
 
 /**
  * Created by aa on 2017/9/5.
  */
+
 @ContentView(R.layout.friend)
 public class Friend_activity extends FragmentActivity {
     private Context mContext;
@@ -34,6 +40,10 @@ public class Friend_activity extends FragmentActivity {
     @ViewInject(R.id.title_bar_name)
     private TextView textView;
 
+    private ViewPager vp;
+    private TabLayout tabLayout;
+    private FriendTopAdapter FtAdapter;
+    private List<Fragment> fragments = new ArrayList<>();
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +52,6 @@ public class Friend_activity extends FragmentActivity {
         //setContentView(R.layout.friend);
         mContext =this;
         x.view().inject(this);
-
         textView.setText("好友消息");
 
         //设置当前页面 首页 字体为红色
@@ -50,7 +59,23 @@ public class Friend_activity extends FragmentActivity {
         Button home =(Button) exFragment.getView().findViewById(button_friend);
         home.setTextColor(Color.parseColor("#f75b47"));
 
+        vp = (ViewPager) findViewById(R.id.FriendTop_viewpage);
+        tabLayout = (TabLayout) findViewById(R.id.FriendTop_tab);
+
+        FtAdapter = new FriendTopAdapter(getSupportFragmentManager());
+        fragments.add(FriendTopFragment.newInstance("0"));
+        fragments.add(FriendTopFragment.newInstance("1"));
+        fragments.add(FriendTopFragment.newInstance("2"));
+        FtAdapter.setFragments(fragments);
+        vp.setAdapter(FtAdapter);
+        //设置tabLayout
+        tabLayout.setupWithViewPager(vp);
+        //设置文字的颜色
+        tabLayout.setTabTextColors(Color.GRAY, Color.BLUE);
+        //设置下划线的颜色
+        tabLayout.setSelectedTabIndicatorColor(Color.BLUE);
     }
+
     @Event(value = {R.id.button_home,R.id.button_message})
     private void event(View view) {
         Intent intent;
---
+++ app/src/main/java/com/example/aa/itravel/adapter/FriendTopAdapter.java
@@ -0,0 +1,41 @@
+package com.example.aa.itravel.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Ynez on 2017/9/9.
+ */
+
+public class FriendTopAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments;
+    private String[] titles = {"通知", "好友", "聊天"};
+
+    public FriendTopAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void setFragments(List<Fragment> fragments) {
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments != null ? fragments.size() : 0;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+
+}
