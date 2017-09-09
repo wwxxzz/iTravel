66ef0ac9cbcb0206bd03abb694e37f3c0f161f3e
wwxxzz <xiaowen1021@foxmail.com>
2017-09-09 12:17:10 +0800

    add Entity preferedtype

---
+++ app/src/main/java/com/example/aa/itravel/tools/PreferredType.java
@@ -0,0 +1,37 @@
+package com.example.aa.itravel.tools;
+
+/**
+ * Created by admin on 2017/9/9.
+ */
+
+public class PreferredType {
+	private Integer preferredid;
+
+	private Integer userid;
+
+	private Integer typeid;
+
+	public Integer getPreferredid() {
+		return preferredid;
+	}
+
+	public void setPreferredid(Integer preferredid) {
+		this.preferredid = preferredid;
+	}
+
+	public Integer getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+
+	public Integer getTypeid() {
+		return typeid;
+	}
+
+	public void setTypeid(Integer typeid) {
+		this.typeid = typeid;
+	}
+}
